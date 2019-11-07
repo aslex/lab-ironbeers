e25493c50ba5a8e4db06c8e16d33145c95bc6c7a
Alexandra Servie <alexservie@Alexandras-MacBook-Pro-2.local>
2019-11-07 15:40:00 +0100

    needs more work

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,10 +9,26 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers);
+      const beersObject = {
+        beerList: beers
+      }
+      res.render('beers.hbs', beersObject);
+
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
 
 
